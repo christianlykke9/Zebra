8eca7da3186b3b44413dfb3f07af48452dab5032
Soren Enevoldsen <senevoldsen90@gmail.com>
2013-03-18 11:26:00 +0100

    Changed main into sequence example

--- source/src/dk/aau/cs/giraf/zebra/MainActivity.java
+++ source/src/dk/aau/cs/giraf/zebra/MainActivity.java
@@ -1,8 +1,12 @@
 package dk.aau.cs.giraf.zebra;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +14,25 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ViewGroup sequenceGroup = (ViewGroup) findViewById(R.id.sequenceViewGroup);
+		
+		int[] drawableIds = new int[] {
+			R.drawable.vask_1,
+			R.drawable.vask_2,
+			R.drawable.vask_3,
+			R.drawable.vask_4
+		};
+		
+		Resources resources = getResources();
+		
+		for (int i = 0; i < drawableIds.length; i++) {
+			Drawable pictoImage = resources.getDrawable(drawableIds[i]);
+			ImageView imageView = new ImageView(getApplication());
+			imageView.setImageDrawable(pictoImage);
+			sequenceGroup.addView(imageView);
+		}
+		
 	}
 
 	@Override
