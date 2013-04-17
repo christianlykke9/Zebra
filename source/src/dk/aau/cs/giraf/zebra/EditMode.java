6af6de7bb9b386c9fd351028afc0560a896ec46a
Soren Enevoldsen <senevoldsen90@gmail.com>
2013-04-17 13:30:59 +0200

    Made EditMode observable

--- source/src/dk/aau/cs/giraf/zebra/EditMode.java
+++ source/src/dk/aau/cs/giraf/zebra/EditMode.java
@@ -1,21 +1,47 @@
 package dk.aau.cs.giraf.zebra;
 
-public class EditMode {
+import java.util.ArrayList;
+
+import android.database.Observable;
+
+public class EditMode extends Observable<EditMode.EditModeObserver> {
 	
-	private static boolean editMode = false;
+	private boolean editMode = false;
+	
+	private static EditMode instance = new EditMode();
+	
+	private EditMode() {
+		//Private Constructor
+	}
 
 	public static boolean get() {
-		return editMode;
+		return instance.editMode;
 	}
 
 	public static void set(boolean mode) {
-		editMode = mode;
-		
+		if (mode != instance.editMode) {
+			instance.editMode = mode;
+			notifyChanged();
+		}
 	}
 	
 	public static void toggle()
 	{
 		set(!get());
+		notifyChanged();
 	}
-
+	
+	protected static void notifyChanged() {
+		ArrayList<EditModeObserver> observers = instance.mObservers;
+		synchronized (observers) {
+			for (int i = observers.size() - 1; i >= 0; i--) {
+				observers.get(i).onEditModeChange(get());
+			}
+		}
+	}
+	
+	public interface EditModeObserver {
+		public void onEditModeChange(boolean editMode);
+	}
+	
 }
