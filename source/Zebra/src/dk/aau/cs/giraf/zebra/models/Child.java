8ae8794b2f7a8301aa236617824172c43b078728
Soren Enevoldsen <senevoldsen90@gmail.com>
2013-04-30 15:40:54 +0200

    Moved models into new package

---
+++ source/Zebra/src/dk/aau/cs/giraf/zebra/models/Child.java
@@ -0,0 +1,54 @@
+package dk.aau.cs.giraf.zebra.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import android.graphics.drawable.Drawable;
+
+public class Child {
+	
+	private long profileId;
+	private String name;
+	private Drawable picture;
+	
+	private List<Sequence> sequences = new ArrayList<Sequence>();
+	
+	public Child(long profileId) {
+		this.profileId = profileId;
+		
+		//TODO: FOR TESTING ONLY:
+		this.name = "Søren Knudsen";
+		
+		//TODO: Get the name from the database
+	}
+	
+	public long getProfileId() {
+		return profileId;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public Drawable getPicture() {
+		return picture;
+	}
+	
+	public List<Sequence> getSequences() {
+		return sequences;
+	}
+	
+	public void setSequences(List<Sequence> sequences) {
+		this.sequences = sequences;
+	}
+	
+	public int getSequenceCount() {
+		return sequences.size();
+	}
+	
+	public void preloadSequenceImages() {
+		
+		// TODO: Logic goes here :]
+	}
+}
