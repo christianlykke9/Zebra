f3391ea6a5e78645d83967b360aef608cc1f089b
Sam Sesptrup Olesen <samolesen@gmail.com>
2013-05-01 11:50:48 +0200

    Revert "Removed alpha when dragging to improve performance"
    This reverts commit dea77112fb671c1b097fc207246bf0039888cea5.

--- source/Zebra/src/dk/aau/cs/giraf/zebra/PictogramView.java
+++ source/Zebra/src/dk/aau/cs/giraf/zebra/PictogramView.java
@@ -100,6 +100,7 @@ public class PictogramView extends LinearLayout {
 	public void liftUp() {
 		pictogram.setScaleX(HIGHLIGHT_SCALE);
         pictogram.setScaleY(HIGHLIGHT_SCALE);
+		this.setAlpha(0.7f);
 		setDeleteButtonVisible(false);
 		invalidate();
 	}
@@ -107,6 +108,7 @@ public class PictogramView extends LinearLayout {
 	public void placeDown() {
 		pictogram.setScaleX(NORMAL_SCALE);
         pictogram.setScaleY(NORMAL_SCALE);
+		this.setAlpha(1.0f);
 		setDeleteButtonVisible(isInEditMode);
 		invalidate();
 	}
