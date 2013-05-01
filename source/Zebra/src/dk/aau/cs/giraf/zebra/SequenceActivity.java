06752daa19b29ac9c804d69824fb02b66e1f3eef
Christian Lykke <christian.lykke@gmail.com>
2013-05-01 14:04:49 +0200

    Deleting new sequences if discarding changes

--- source/Zebra/src/dk/aau/cs/giraf/zebra/SequenceActivity.java
+++ source/Zebra/src/dk/aau/cs/giraf/zebra/SequenceActivity.java
@@ -139,7 +139,9 @@ public class SequenceActivity extends Activity {
 	
 	private void discardChangesAndReturn() {
 		// If the sequence is new and the changes are discarded, the sequence will be deleted.
-		child.getSequences().remove(originalSequence);
+		if (isNew) {
+			child.getSequences().remove(originalSequence);
+		}
 		
 		// Returning to the overview activity
 		finish();
