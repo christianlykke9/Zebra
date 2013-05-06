107e38202bffd03e552d56c00df3d021f588d60a
Soren Enevoldsen <senevoldsen90@gmail.com>
2013-05-06 13:21:51 +0200

    Smoother corners in RoundedImageView

--- source/Zebra/src/dk/aau/cs/giraf/zebra/RoundedImageView.java
+++ source/Zebra/src/dk/aau/cs/giraf/zebra/RoundedImageView.java
@@ -3,44 +3,46 @@ package dk.aau.cs.giraf.zebra;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
-import android.graphics.Path;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
 
+/**
+ * Rounds the corner of the image set.
+ * 
+ * Maybe cut of parts of the image.
+ */
 public class RoundedImageView extends ImageView {
-	
-	private static final int DEFAULT_CORNER_RADIUS = 10;
+
+	private static final int DEFAULT_CORNER_RADIUS = -1;
 	
 	private float cornerRadius;
+	
+	RectF imageRect = new RectF();
+	Paint opacity = new Paint();
+	
  
     public RoundedImageView(Context context, float radius) {
         super(context);
-        
         cornerRadius = radius;
         ensureClipPathSupport();
     }
     
     public RoundedImageView(Context context, AttributeSet attrs) {
-    	super(context, attrs);
-    	
-		cornerRadius = getCornerRadius(context, attrs);
-		ensureClipPathSupport();
-    }
- 
-    public RoundedImageView(Context context, AttributeSet attrs, float radius) {
-        this(context, attrs);
-        
+        super(context, attrs);
         cornerRadius = getCornerRadius(context, attrs);
         ensureClipPathSupport();
     }
  
-    public RoundedImageView(Context context, AttributeSet attrs, int defStyle, float radius) {
+    public RoundedImageView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
-
-        cornerRadius = radius;
+        cornerRadius = getCornerRadius(context, attrs);
         ensureClipPathSupport();
     }
     
@@ -69,14 +71,24 @@ public class RoundedImageView extends ImageView {
  
     @Override
     protected void onDraw(Canvas canvas) {
-        Path clipPath = new Path();
-        RectF rect = new RectF(0, 0, this.getWidth(), this.getHeight());
-        clipPath.addRoundRect(rect, cornerRadius, cornerRadius, Path.Direction.CW);
-        canvas.clipPath(clipPath);
+    	int width = getWidth() - getPaddingLeft() - getPaddingRight();
+    	int height = getHeight() - getPaddingTop() - getPaddingBottom();
+    	
+    	float fCornerRadius = cornerRadius;
+    	if (cornerRadius == -1) 
+    		fCornerRadius = Math.min(width, height) / 11.f;
+
+    	imageRect.set(0, 0, width, height);
+    	
+    	opacity.setFlags(Paint.ANTI_ALIAS_FLAG);
+        opacity.setColor(Color.RED);
         
-    	canvas.drawColor(0x28FFFFFF);
+        canvas.drawRoundRect(imageRect, fCornerRadius, fCornerRadius, opacity);
+        opacity.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.saveLayer(imageRect, opacity, Canvas.ALL_SAVE_FLAG);
         
         super.onDraw(canvas);
         
+        canvas.restore();
     }    
 }
