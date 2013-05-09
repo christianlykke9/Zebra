9b7252c38945dd939792b2bc87262d17c1c2f9c3
Søren Knudsen <k@fuse.dk>
2013-05-09 16:33:09 +0200

    RoundedImageView fix??

--- source/Zebra/src/dk/aau/cs/giraf/zebra/RoundedImageView.java
+++ source/Zebra/src/dk/aau/cs/giraf/zebra/RoundedImageView.java
@@ -7,7 +7,11 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.Xfermode;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
@@ -71,24 +75,47 @@ public class RoundedImageView extends ImageView {
  
     @Override
     protected void onDraw(Canvas canvas) {
-    	int width = getWidth() - getPaddingLeft() - getPaddingRight();
-    	int height = getHeight() - getPaddingTop() - getPaddingBottom();
-    	
-    	float fCornerRadius = cornerRadius;
-    	if (cornerRadius == -1) 
-    		fCornerRadius = Math.min(width, height) / 11.f;
-
-    	imageRect.set(0, 0, width, height);
-    	
-    	opacity.setFlags(Paint.ANTI_ALIAS_FLAG);
-        opacity.setColor(Color.RED);
-        
-        canvas.drawRoundRect(imageRect, fCornerRadius, fCornerRadius, opacity);
-        opacity.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.saveLayer(imageRect, opacity, Canvas.ALL_SAVE_FLAG);
-        
-        super.onDraw(canvas);
-        
-        canvas.restore();
+    	// Round some corners betch!
+    			Drawable maiDrawable = getDrawable();
+    			if (maiDrawable instanceof BitmapDrawable) {
+    				
+    				int width = getWidth() - getPaddingLeft() - getPaddingRight();
+    		    	int height = getHeight() - getPaddingTop() - getPaddingBottom();
+    		    	
+    		    	float fCornerRadius = cornerRadius;
+    		    	if (cornerRadius == -1) 
+    		    		fCornerRadius = Math.min(width, height) / 11.f;
+    				
+    				Paint paint = ((BitmapDrawable) maiDrawable).getPaint();
+    		        final int color = 0xff000000;
+    		        Rect bitmapBounds = maiDrawable.getBounds();
+    		        final RectF rectF = new RectF(bitmapBounds);
+    		        
+    		        // Create an off-screen bitmap to the PorterDuff alpha blending to work right
+    				int saveCount = canvas.saveLayer(rectF, null,
+    	                    Canvas.MATRIX_SAVE_FLAG |
+    	                    Canvas.CLIP_SAVE_FLAG |
+    	                    Canvas.HAS_ALPHA_LAYER_SAVE_FLAG |
+    	                    Canvas.FULL_COLOR_LAYER_SAVE_FLAG |
+    	                    Canvas.CLIP_TO_LAYER_SAVE_FLAG);
+    				
+    				// Resize the rounded rect we'll clip by this view's current bounds
+    				// (super.onDraw() will do something similar with the drawable to draw)
+    				getImageMatrix().mapRect(rectF);
+    	 
+    		        paint.setAntiAlias(true);
+    		        canvas.drawARGB(0, 0, 0, 0);
+    		        paint.setColor(color);
+    		        canvas.drawRoundRect(rectF, fCornerRadius, fCornerRadius, paint);
+    	 
+    				Xfermode oldMode = paint.getXfermode();
+    				// This is the paint already associated with the BitmapDrawable that super draws
+    		        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+    		        super.onDraw(canvas);
+    		        paint.setXfermode(oldMode);
+    		        canvas.restoreToCount(saveCount);
+    			} else {
+    				super.onDraw(canvas);
+    			}
     }    
 }
